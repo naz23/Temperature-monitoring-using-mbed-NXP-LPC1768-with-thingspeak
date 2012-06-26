683b7f28670b1a6b4a5dda756b798c980cc17f0a
donatien <donatien@localhost>
2012-06-26 15:34:26 +0000

    Switched from uint8_t* to char* for data type

--- main.cpp
+++ main.cpp
@@ -11,15 +11,15 @@ int main()
     TCPSocket sock;
     sock.connect("mbed.org", 80);
     
-    const char http_cmd[] = "GET /media/uploads/donatien/hello.txt HTTP/1.1\r\nHost: %s\r\n\r\n";
-    sock.send((std::uint8_t*)http_cmd, sizeof(http_cmd) - 1, 3000);
+    char http_cmd[] = "GET /media/uploads/donatien/hello.txt HTTP/1.1\r\nHost: %s\r\n\r\n";
+    sock.send(http_cmd, sizeof(http_cmd) - 1, 3000);
 
     char in_buf[256];      
     bool firstIteration = true;
     int ret;
     do
     {
-        ret = sock.receive((std::uint8_t*)in_buf, 255, firstIteration?3000:0);
+        ret = sock.receive(in_buf, 255, firstIteration?3000:0);
         in_buf[ret] = '\0';
         
         printf("Received %d chars from server: %s\n", ret, in_buf);
