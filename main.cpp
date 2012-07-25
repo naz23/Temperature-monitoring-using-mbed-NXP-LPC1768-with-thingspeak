0fca1533b1c0afea119b2a0aa9eef118ee8e38a4
emilmont <emilmont@localhost>
2012-07-25 15:00:43 +0000

    Update EthernetInterface

--- main.cpp
+++ main.cpp
@@ -1,34 +1,32 @@
 #include "mbed.h"
 #include "EthernetInterface.h"
 
-int main() 
-{
+int main() {
     EthernetInterface eth;
     eth.init(); //Use DHCP
     eth.connect();
     printf("IP Address is %s\n", eth.getIPAddress());
     
-    TCPSocket sock;
+    TCPSocketConnection sock;
     sock.connect("mbed.org", 80);
     
     char http_cmd[] = "GET /media/uploads/donatien/hello.txt HTTP/1.1\r\nHost: %s\r\n\r\n";
-    sock.send(http_cmd, sizeof(http_cmd) - 1, 3000);
-
-    char in_buf[256];      
-    bool firstIteration = true;
-    int ret;
-    do
-    {
-        ret = sock.receive(in_buf, 255, firstIteration?3000:0);
-        in_buf[ret] = '\0';
-        
-        printf("Received %d chars from server: %s\n", ret, in_buf);
-        firstIteration = false;
-    } while( ret > 0 );
-      
-    sock.close();  
+    sock.send_all(http_cmd, sizeof(http_cmd) - 1, 3000);
     
-    eth.disconnect();  
+    char in_buf[256];
+    int ret;
+    while (true) {
+        ret = sock.receive(in_buf, 255, 100);
+        if (ret <= 0)
+            break;
+        
+        in_buf[ret] = '\0';
+        printf("Received %d chars from server: %s\n", ret, in_buf);
+    }
+      
+    sock.close();
+    
+    eth.disconnect();
     
     while(1) {
     }
